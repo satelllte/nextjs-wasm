3684963fa4f2d4b8531c2e0a2af56529c71d575a
Egor Mesyats <lunaerxs@gmail.com>
2022-05-22 17:10:45 +0000

    first canvas render

--- src/components/WASMExample.tsx
+++ src/components/WASMExample.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { WASMContext } from "../context/WASM"
 
 export const WASMExample = () => {
@@ -8,5 +8,26 @@ export const WASMExample = () => {
     return <>...</>
   }
 
-  return <>Computed from WASM: 4+3={ctx.wasm.add(4,3)}</>
+  return <Canvas wasm={ctx.wasm}/>
+}
+
+const Canvas: React.FC<CanvasProps> = ({ wasm }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    if (!canvasRef.current) return
+
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+
+    if (!ctx) return
+
+    wasm.draw(ctx, canvas.width, canvas.height)
+  }, [])
+
+  return <canvas ref={canvasRef}/>
+}
+
+interface CanvasProps {
+  wasm: typeof import('wasm')
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,29 @@
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::prelude::JsValue;
+use wasm_bindgen::Clamped;
+use web_sys::CanvasRenderingContext2d;
+use web_sys::ImageData;
+
+#[wasm_bindgen]
+pub fn draw(
+    ctx: &CanvasRenderingContext2d,
+    width: u32,
+    height: u32,
+) -> Result<(), JsValue> {
+    let mut data: Vec<u8> = Vec::new();
+
+    for _x in 0..width {
+        for _y in 0..height {
+            data.push(50);
+            data.push(120);
+            data.push(155);
+            data.push(255);
+        }
+    }
+
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut data), width, height)?;
+    return ctx.put_image_data(&data, 0.0, 0.0);
+}
 
 #[wasm_bindgen]
 pub fn add(x: i32, y: i32) -> i32 {
