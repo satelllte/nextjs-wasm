7ff260ea7bed371010b843d9295a8e818830fd62
George Satellite <lunaerxs@gmail.com>
2022-05-23 21:28:36 +0000

    codespaces: WIP 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions.poll = 1000
+    config.watchOptions.aggregateTimeout = 300
+    return config
+  },
 }
 
 module.exports = nextConfig
