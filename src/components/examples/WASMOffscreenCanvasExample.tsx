879b9dff67bfe5760d83af3199c61b409adf2693
George Satellite <lunaerxs@gmail.com>
2022-05-25 11:30:54 +0000

    WASMOffscreenCanvasExample: 1

--- src/components/examples/WASMOffscreenCanvasExample.tsx
+++ src/components/examples/WASMOffscreenCanvasExample.tsx
@@ -1,3 +1,31 @@
+import { useContext, useRef } from "react"
+import { WASMWorkerContext } from "../../context/WASMWorker"
+import { WASMWorker } from "../../workers/wasm/types"
+
 export const WASMOffscreenCanvasExample = () => {
-  return <>I'm WASMOffscreenCanvasExample</>
+  const ctx = useContext(WASMWorkerContext)
+
+  if (!ctx.loaded) {
+    return <>...</>
+  }
+
+  if (!ctx.success) {
+    return <>OffscreenCanvas is not supported</>
+  }
+
+  if (!ctx.worker) {
+    return <>Unexpected error occured during WASMWorker load</>
+  }
+
+  return <Canvas worker={ctx.worker} />
+}
+
+const Canvas: React.FC<CanvasProps> = ({ worker }) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+  return <canvas width={600} height={400} ref={canvasRef}/>
+}
+
+interface CanvasProps {
+  worker: WASMWorker
 }
