e3dc01087f375f7dd0ee6fb499db5753e68c28ec
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-17 16:50:16 +0100

    Fix "memory access out of bounds" error
    See https://github.com/rustwasm/wasm-bindgen/issues/3153 for context

--- src/context/WASM.tsx
+++ src/context/WASM.tsx
@@ -1,6 +1,18 @@
 import { ReactNode, useEffect, useState } from 'react'
 import { createContext } from 'react'
 
+// initialize at module scope, to prevent executing twice on strict mode,
+// which can cause issues when calling async functions on wasm
+const wasmPromise = import('wasm').then(wasm => {
+  if (typeof window !== "undefined") { 
+      // client side: initialize
+      return wasm.default().then(() => wasm)
+  } else { 
+      // server side: do nothing
+      return wasm
+  }
+}) 
+
 const initial: IWASMContext = {}
 
 export const WASMContext = createContext(initial)
@@ -12,8 +24,7 @@ export const WASMContextProvider: React.FC<WASMContextProviderProps> = ({
 
   useEffect(() => {
     (async() => {
-      const wasm = await import('wasm')
-      await wasm.default()
+      const wasm = await wasmPromise
       setState({ wasm })
     })()
   }, [])
